9677370239e2f11ad2b28865cd897197c25e6c96
NestorFabianCabrera <nestor.cabrera@gopenux.com>
2024-08-22 14:05:58 -0500

    Config 22/08 14:00

--- app.py
+++ app.py
@@ -6,11 +6,13 @@ app = Flask(__name__)
 app.config['MONGO_URI'] = 'mongodb://mongo:27017/mydatabase'
 mongo = PyMongo(app)
 
+# Principal donde se muestra la vista del template
 @app.route('/')
 def index():
     items = mongo.db.items.find()
     return render_template('index.html', items=items)
 
+# Metodo post para poder insertar items
 @app.route('/add', methods=['POST'])
 def add_item():
     name = request.form.get('name')
@@ -18,6 +20,7 @@ def add_item():
     mongo.db.items.insert_one({'name': name, 'value': value})
     return redirect(url_for('index'))
 
+# Metodo post para ver los de la actualización
 @app.route('/edit/<item_id>', methods=['POST'])
 def edit_item(item_id):
     name = request.form.get('name')
@@ -25,6 +28,7 @@ def edit_item(item_id):
     mongo.db.items.update_one({'_id': ObjectId(item_id)}, {'$set': {'name': name, 'value': value}})
     return redirect(url_for('index'))
 
+# Método para eliminar los items que se muestran
 @app.route('/delete/<item_id>', methods=['POST'])
 def delete_item(item_id):
     mongo.db.items.delete_one({'_id': ObjectId(item_id)})
